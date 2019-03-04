4f9dfcdfef9d920586ea4b92493100f5bf7aa050
Brendan Palmer <30694114+bapalmer@users.noreply.github.com>
2019-03-04 14:50:13 +0000

    Update install.R

--- install.R
+++ install.R
@@ -1,3 +1,3 @@
-install.packages(c("readr", "tidyr", "dplyr", "magrittr", "ggplot2", "tibble"))
+install.packages(c("readr", "tidyr", "dplyr", "magrittr", "ggplot2", "tibble", "stargazer"))
 # install.packages("pacman") # This allows you to load specific versions of R packages using p_install_version()
 
