d9ee9965a63c0f44b63bd5cf58ce837dab344863
Brendan Palmer <30694114+bapalmer@users.noreply.github.com>
2019-03-04 16:00:22 +0000

    Delete install.R

--- install.R
+++
@@ -1,5 +0,0 @@
-install.packages("tidyverse")
-install.packages("rmarkdown")
-install.packages("stargazer")
-# install.packages("pacman") # This allows you to load specific versions of R packages using p_install_version()
-
